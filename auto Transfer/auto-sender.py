9d27dcb94dacf291eca9313aa3800ca2166a06c0
sobhan shokri <xd3vman@gmail.com>
2023-01-20 06:18:05 +0330

    Update auto-sender.py
    Added error handling

--- auto Transfer/auto-sender.py
+++ auto Transfer/auto-sender.py
@@ -38,7 +38,7 @@ account_2 = "0x858C1eAC770C883f6FB4496B856023A7A483b6B6"
 private_key = "3ddc37fdcd6ba99f9a16206467e459d4009ab30b3b4909a5ba0ca50ea40d2624"
 
 balance=0
-gas_fee = 21000*56
+gas_fee = 21000*56  # You can change gasfee value
 gas_fee = Decimal(gas_fee)
 gas_fee = web3.fromWei(gas_fee,'Gwei')
 print("get balance...","gas fee is : ", gas_fee)
@@ -47,34 +47,45 @@ def get_balance_loop():
     while True:
         while 0.0005>balance:
             #Get balance account
-            balance = web3.eth.getBalance(account_1)
-            balance = web3.fromWei(balance, "ether") #convert to ether value
+            try:
+                balance = web3.eth.getBalance(account_1)
+                balance = web3.fromWei(balance, "ether") #convert to ether value
+            except:
+                print("error , i can't get balance...")
 
-        balance = balance-gas_fee
-        print(balance)
-        # print(balance)
-        # print(web3.fromWei(balance, "ether"))
-        build_transaction(balance)
-        
+        try:
+            balance = balance-gas_fee
+            print(balance)
+            # print(balance)
+            # print(web3.fromWei(balance, "ether"))
+            build_transaction(balance)
+        except:
+            print("Error, check balance and Gasfee again")
+            
 
 
 def build_transaction(balance):
-    #get nonce number
-    nonce = web3.eth.getTransactionCount(account_1)
-#build transaction
-    tx = {
-        'nonce':nonce,
-        'to':account_2,
-        'value':web3.toWei(balance,'ether'),
-        'gas':21000,
-        'gasPrice':web3.toWei('56','gwei')
-    }
+    try:
+        #get nonce number
+        nonce = web3.eth.getTransactionCount(account_1)
+        #build transaction
+        tx = {
+            'nonce':nonce,
+            'to':account_2,
+            'value':web3.toWei(balance,'ether'),
+            'gas':21000,
+            'gasPrice':web3.toWei('56','gwei')
+        }
 
-    #sign transaction with private key
-    signed_tx = web3.eth.account.signTransaction(tx,private_key)
-    #send Transaction
-    tx_hash= web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+        #sign transaction with private key
+        signed_tx = web3.eth.account.signTransaction(tx,private_key)
+        #send Transaction
+        tx_hash= web3.eth.sendRawTransaction(signed_tx.rawTransaction)
 
-    print(web3.toHex(tx_hash))
-    get_balance_loop()
+        print(web3.toHex(tx_hash))
+        print("Transaction Completed\n GET Balance Again...")
+        get_balance_loop()
+    except:
+        print("ERROR, check buldTransction Funcation")
+
 get_balance_loop()
